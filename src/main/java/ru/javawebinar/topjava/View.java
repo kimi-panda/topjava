f2f60d6115fb1e167872d39b72c9b1eb31cceb74
kimi-panda <n@limonova.net>
2020-01-05 16:03:13 +0500

    Revert "10 05 persist validate group"
    This reverts commit d039f06b

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,7 +1,10 @@
 package ru.javawebinar.topjava;
 
-import javax.validation.groups.Default;
-
 public class View {
-    public interface Persist extends Default {}
+    public interface JsonREST {}
+    public interface JsonUI {}
+
+    // https://narmo7.wordpress.com/2014/04/26/how-to-set-up-validation-group-in-springmvc/
+    // http://forum.spring.io/forum/spring-projects/web/117289-validated-s-given-groups-should-consider-default-group-or-not
+    public interface ValidatedUI {}
 }
